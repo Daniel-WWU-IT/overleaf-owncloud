25553068b06b090593d8c7da9228663faf0d83c6
Daniel Müller <daniel.mueller@uni-muenster.de>
2022-11-21 11:49:53 +0100

    Adjust wrapper height

--- js/launcher/launcher.js
+++ js/launcher/launcher.js
@@ -1,9 +1,22 @@
 'use strict';
 
 (function (OC, window, $) {
+    const wrapperHeightBuffer = 50;
+
     $(document).ready(function () {
-        $('#overleaf-wrapper').on('load', function () {
+        $('#overleaf-wrapper').on('load', () => {
             $('#overleaf-wrapper-loading').hide();
         });
+
+        $(window).on('resize', () => {
+            _updateWrapperHeight();
+        });
+
+        _updateWrapperHeight();
     });
+
+    function _updateWrapperHeight() {
+        let winHeight = $(window).height();
+        $('#overleaf-wrapper').height(winHeight - wrapperHeightBuffer);
+    }
 })(OC, window, jQuery);
